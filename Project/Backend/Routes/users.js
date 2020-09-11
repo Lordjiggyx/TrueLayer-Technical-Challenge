40c92e7c19a8c4aee2f573788f2b042d03bfbe1c
Tomi Ilori <t84168550@china.huawei.com>
2020-09-11 13:01:29 +0100

    Server routes & test files created with scripts

---
+++ Project/Backend/Routes/users.js
@@ -0,0 +1,9 @@
+//Bringing in express
+const express = require("express");
+const { Router } = require("express");
+//Using express router 
+const router = express.Router();
+
+
+//mkaing thsi file exportble as middleware to be used by express 
+module.exports = router;
---
+++ Project/Backend/server.js
@@ -0,0 +1,23 @@
+//Express used to create server
+const express = require("express");
+
+//Routes 
+const Users =require("./Routes/users");
+
+//Initialise express
+const app = express();
+
+//Body Parser MiddleWare
+app.use(express.json());
+
+//Create a port Variable
+const port = process.env.PORT || 5000 ; 
+
+//Middleware for using the user route
+app.use("/Routes" , Users);
+
+//Starting the server on localhost
+app.listen(port, ()=>
+{
+    console.log(`Server started on ${port}`)
+})
