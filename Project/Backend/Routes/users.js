a41184440cdfa257520f3bc0f539b8a7b305ccc1
Tomi Ilori <t84168550@china.huawei.com>
2020-09-13 21:33:21 +0100

    added comments

--- Project/Backend/Routes/users.js
+++ Project/Backend/Routes/users.js
@@ -4,8 +4,15 @@ const express = require("express");
 //Using express router 
 const router = express.Router();
 
+/*router is used to ensure that methods are only invoked if the URL path matches exactly the string passed in
+I use arrow functions which take in a response and request object
+
+after router. , i can use POST GET PUT or DELETE depending on the request
+*/
+
 router.get("/" , (req , res)=>
 {
+    //I use the response objects .send() method to send a string
     res.send("Hello")
 })
 
@@ -19,5 +26,7 @@ router.get("/sendJSONBody" , (req , res)=>
 {
     res.send({msg:"TrueLayer Is The Best"})
 })
+
+
 //making this file exportble as middleware to be used by express 
 module.exports = router;
