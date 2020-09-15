75af67e53708ab80f77f01c3f53a476ecbb6bbdc
Tomi Ilori <t84168550@china.huawei.com>
2020-09-15 09:24:03 +0100

    Search functionality complete

--- Project/client/src/Components/Home.js
+++ Project/client/src/Components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { FormGroup, Label, Input, Container, Button , Alert} from "reactstrap"
+import { FormGroup, Label, Input, Container, Button , Alert ,Modal, ModalHeader, ModalBody, ModalFooter} from "reactstrap"
+import axios from "axios"
 import "../CSS/Home.css"
 export default class Home extends Component {
 
@@ -18,21 +19,32 @@ export default class Home extends Component {
         
     }
 
+    modalToggle =()=>
+    {
+        this.setState({
+            modalVisibility:!this.state.modalVisibility
+        })
+
+        
+    }
+
     getPokemon = () =>
     {
         if(this.state.name === "")
         {
-            this.setState({EmptyError:true} , 
-            ()=>
-            {
-                window.setTimeout(3000)
-            })
+            this.setState({EmptyError:true})
         }
         else
         {
-            console.log("success")
-            this.setState({EmptyError:false})
+            axios.get(`Routes/pokemon/${this.state.name}`)
+            .then(res =>
+                {
+                    this.setState({name:"",pokemon:res.data, modalVisibility:true})
+                    document.getElementById("name").value = "";
+                })
         }
+
+       
     }
     
     render() {
@@ -46,7 +58,7 @@ export default class Home extends Component {
                 </div>
 
                 <div className="vertical-center">
-                <FormGroup>
+                <FormGroup id="form1">
                     <Label for = "Name"><b>Name:</b></Label>
                     <Input  type="text" name = "name "id="name" placeholder="Enter A Name" onChange={this.handleChange("name")}  ></Input>
 
@@ -63,6 +75,24 @@ export default class Home extends Component {
 
                 
                 </Container>
+
+                <Modal isOpen={this.state.modalVisibility}
+                toggle={this.modalToggle}>
+                <ModalHeader >Pokemon Information</ModalHeader>
+                <div className="text-center">
+                <ModalBody>                
+                <h4>Pokemon Name</h4>
+                <div>{this.state.pokemon.name}</div>
+
+                <h4>Pokemon Description</h4>
+                <div>{this.state.pokemon.desc}</div>
+
+                </ModalBody>
+                </div>
+                <ModalFooter>
+                    <Button color="primary" onClick={this.modalToggle}>Close</Button>
+                </ModalFooter>
+                </Modal>
             
         </div>
         )
